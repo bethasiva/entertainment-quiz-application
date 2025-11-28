b7f84863127738357b445a5db2df9fe5363e7528
bethasiva <94673363+bethasiva@users.noreply.github.com>
2025-11-29 04:09:07 +0530

    Create envData.ts with application constants
    Added environment variables for application configuration.

---
+++ src/constants/envData.ts
@@ -0,0 +1,8 @@
+APP_BASE_URL="https://entertainment-quiz-application-henna.vercel.app"
+# ENDPOINTS TO FETCH QUIZ DATA
+QUIZ_QUESTIONS_API_ENDPOINT="https://the-trivia-api.com/v2/questions"
+PG_DATABASE_HOST=dpg-d4kq0gi4d50c73dm39j0-a.frankfurt-postgres.render.com
+PG_DATABASE_NAME=postgres_1r0h
+PG_DATABASE_USERNAME=postgres_1r0h_user
+PG_DATABASE_PASSWORD=wGTuu0TAQH131G84LAWmsZy60reCB3ps
+PG_DATABASE_PORT=5432
